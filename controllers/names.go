7dbc28cdb5cf2f98262d5663531367de01d956c6
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-30 10:43:09 -0300

    Create controller to get names generated

---
+++ controllers/names.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"net/http"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/diegovictor/name-generator/helpers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetNames(c *gin.Context) {
+	dataset := c.Query("dataset")
+
+	params := []string{"scripts/main.py", dataset}
+	out, err := exec.Command("python", params...).Output()
+
+	if !helpers.Catch(err, c) {
+		text := helpers.ToUtf8(out)
+		names := regexp.MustCompile("\r|\n").ReplaceAllString(text, "")
+		result := strings.Split(names, ",")
+
+		c.IndentedJSON(http.StatusOK, result)
+	}
+}
