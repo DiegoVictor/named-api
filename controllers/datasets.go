3adc283e74648e86ec4878ae257e494ce777b8f8
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-01-30 10:42:20 -0300

    Create controller to retrieve datasets configs

---
+++ controllers/datasets.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/diegovictor/name-generator/helpers"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Dataset struct {
+	Id       int    `json:"id"`
+	Category string `json:"category"`
+	Color    string `json:"color"`
+	Title    string `json:"title"`
+}
+
+func GetDatasets(c *gin.Context) {
+	data, err := ioutil.ReadFile("./config/datasets.json")
+
+	if !helpers.Catch(err, c) {
+		var parsedJson []Dataset
+
+		json.Unmarshal([]byte(data), &parsedJson)
+		c.IndentedJSON(http.StatusOK, parsedJson)
+	}
+}
